186ae1c7ba178a20acc65e5394b41d3a897ebc28
Wojciech Malarski <w.malarski@gmail.com>
2023-01-22 22:15:06 +0100

    adds open layer simple example

---
+++ src/components/MapCard/MapCard.tsx
@@ -0,0 +1,35 @@
+import { Map, View } from "ol";
+import TileLayer from "ol/layer/Tile";
+import OSM from "ol/source/OSM";
+import { Component, createEffect, createSignal } from "solid-js";
+
+export const MapCard: Component = () => {
+  if (typeof window === "undefined") {
+    return <div id="map" />;
+  }
+
+  const [ref, setRef] = createSignal<HTMLDivElement>();
+
+  const map = new Map({
+    target: "map",
+    layers: [
+      new TileLayer({
+        source: new OSM(),
+      }),
+    ],
+    view: new View({
+      center: [0, 0],
+      zoom: 2,
+    }),
+  });
+
+  createEffect(() => {
+    map.setTarget(ref());
+  });
+
+  console.log({ map });
+
+  return (
+    <div style={{ width: "500px", height: "500px" }} ref={setRef} id="map" />
+  );
+};
--- src/root.tsx
+++ src/root.tsx
@@ -22,7 +22,7 @@ export default function Root() {
         <Meta charset="utf-8" />
         <Meta name="viewport" content="width=device-width, initial-scale=1" />
       </Head>
-      <Body>
+      <Body style={{ height: "100vh", "overflow-y": "clip", margin: "0" }}>
         <Suspense>
           <ErrorBoundary>
             <A href="/">Index</A>
